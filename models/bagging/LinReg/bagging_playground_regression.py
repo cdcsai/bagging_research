791fa5f15bcbb3a8474c27bed5eb12b569a079df
charlesdognin <charles.dognin@polytechnique.edu>
2019-04-29 13:17:02 -0400

    changing with training loop on top

--- models/bagging/LinReg/bagging_playground_regression.py
+++ models/bagging/LinReg/bagging_playground_regression.py
@@ -33,20 +33,19 @@ def bagging(x, y):
 
 if __name__ == "__main__":
     from tqdm import tqdm
+    from collections import defaultdict
     parser = argparse.ArgumentParser(description='TLBiLSTM network')
-    parser.add_argument('--seed', type=int, default=0)
+    # parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--noise', type=float, default=0.5)
-    parser.add_argument('--ds', type=str, default='cali', help="Dataset")
+    parser.add_argument('--ds', type=str, default='def', help="Dataset")
     parser.add_argument('--n_feats', type=int, default=2, help="num features")
-    parser.add_argument('--n_average', type=int, default=500, help="Number or average")
-    parser.add_argument('--n_samples', type=int, default=1000, help="NUmber of samples")
+    parser.add_argument('--n_average', type=int, default=50, help="Number or average")
+    parser.add_argument('--n_samples', type=int, default=100, help="NUmber of samples")
+    parser.add_argument('--n_train_iter', type=int, default=5, help="Number of train/test loop")
 
     args = parser.parse_args()
     print("\n" + "Arguments are: " + "\n")
     print(args)
-    random.seed(args.seed)
-    np.random.seed(args.seed)
-    dico = dict()
 
 
     # Loading datasets
@@ -58,50 +57,52 @@ if __name__ == "__main__":
     elif args.ds == 'cali':
         X, y = fetch_california_housing()['data'], fetch_california_housing()['target']
     else:
-        X, y = make_regression(n_samples=args.n_samples, n_features=args.n_feats, noise=args.noise, random_state=args.seed)
+        X, y = make_regression(n_samples=args.n_samples, n_features=args.n_feats, noise=args.noise, random_state=0)
 
-    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.60, random_state=args.seed)
-    print(len(x_train), len(x_test))
+    dico = defaultdict(list)
+    for itr_train in tqdm(range(args.n_train_iter)):
+        x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.95, random_state=itr_train)
+        print(len(x_train), len(x_test))
 
-    # Without Bagging
-    mean_mse_wob = []
-    for i in range(args.n_average):
-        predictions = []
-        reg = LinearRegression()
-        reg.fit(x_train, y_train)
-        predictions.append(reg.predict(x_test))
-        predictions = np.array(predictions)
-
-        # Testing
-        final_pred_ = np.mean(predictions, axis=0)
-        assert len(final_pred_) == len(y_test)
-        mse = mean_squared_error(y_test, final_pred_)
-        mean_mse_wob.append(mse)
-    mse_wob = np.mean(mean_mse_wob)
-
-    for N in tqdm(range(5, 100)):
-        mean_mse = []
+        # Without Bagging
+        mean_mse_wob = []
         for i in range(args.n_average):
             predictions = []
-            for tr in range(N):
-                x_train_, y_train_ = bagging(x_train, y_train)
-                assert len(x_train_) == len(y_train_)
-                reg = LinearRegression()
-                reg.fit(x_train_, y_train_)
-                predictions.append(reg.predict(x_test))
+            reg = LinearRegression()
+            reg.fit(x_train, y_train)
+            predictions.append(reg.predict(x_test))
             predictions = np.array(predictions)
 
             # Testing
             final_pred_ = np.mean(predictions, axis=0)
             assert len(final_pred_) == len(y_test)
             mse = mean_squared_error(y_test, final_pred_)
-            mean_mse.append(mse)
+            mean_mse_wob.append(mse)
+        dico[0].append(np.mean(mean_mse_wob))
 
-        dico[N] = np.mean(mean_mse)
+        for N in tqdm(range(5, 100)):
+            mean_mse = []
+            for i in range(args.n_average):
+                predictions = []
+                for tr in range(N):
+                    x_train_, y_train_ = bagging(x_train, y_train)
+                    assert len(x_train_) == len(y_train_)
+                    reg = LinearRegression()
+                    reg.fit(x_train_, y_train_)
+                    predictions.append(reg.predict(x_test))
+                predictions = np.array(predictions)
+
+                # Testing
+                final_pred_ = np.mean(predictions, axis=0)
+                assert len(final_pred_) == len(y_test)
+                mse = mean_squared_error(y_test, final_pred_)
+                mean_mse.append(mse)
+
+            dico[N].append(np.mean(mean_mse))
 
     print('finish')
     with open(f'results_mse_{args.ds}|{args.noise}|{args.n_average}|{args.n_feats}|{args.n_samples}.txt', 'w') as f:
-        f.write('mse_without_b |' + str(mse_wob) + '\n')
         for key, value in dico.items():
-            f.write(str(key) + '|' + str(value) + '\n')
+            assert len(value) == args.n_train_iter
+            f.write(str(key) + '|' + str(np.mean(value)) + '\n')
         f.close()
