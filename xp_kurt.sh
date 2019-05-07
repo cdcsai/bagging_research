38b1c70de0f1c8966830487250a16dfcf3a87058
charlesdognin <charles.dognin@polytechnique.edu>
2019-05-06 22:35:30 -0400

    updating xps

--- xp_kurt.sh
+++ xp_kurt.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
-python experiment_3.py --n_trials=100000 --n=10 --N=100 --inv_a=8 &&
-#python experiment_3.py --n_trials=100000 --n=10 --N=50 --inv_a=8 &&
-#python experiment_3.py --n_trials=100000 --n=10 --N=70 --inv_a=8
 
-python experiment_3.py --n_trials=100000 --n=10 --N=100 --inv_a=12 &&
+python experiment_3.py --n_trials=50000 --n=10 --N=150 --inv_a=12 &&
 #python experiment_3.py --n_trials=100000 --n=10 --N=50 --inv_a=12 &&
 #python experiment_3.py --n_trials=100000 --n=10 --N=70 --inv_a=12
 
-python experiment_3.py --n_trials=100000 --n=10 --N=100 --inv_a=16
+python experiment_3.py --n_trials=50000 --n=10 --N=200 --inv_a=16
 #python experiment_3.py --n_trials=100000 --n=10 --N=50 --inv_a=16 &&
 #python experiment_3.py --n_trials=100000 --n=10 --N=70 --inv_a=16
