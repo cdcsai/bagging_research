1ba4418a659b6009673dfc09690aa28bb5415efb
charles <charles.dognin@verisk.com>
2019-05-10 18:53:44 -0400

    change plots

--- plot_experiment_1.py
+++ plot_experiment_1.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
 
     csv = False
 
-    data = open('results_mse_def|5.0|5|10|2000_mlp_test_size=0.1.txt',
+    data = open('results_mse_def|5.0|50|2|1000_decision_tree_test_size=0.95.txt',
                 'r').readlines()
     x, y1, y2 = [int(el.split('|')[0]) for el in data[1:]], \
                 [float(el.split('|')[1]) for el in data[1:]], float(data[0].split('|')[1])
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     ax.plot(x, fittedData, linestyle='-', color='#900000', label="fit with ({0:0.2g},{1:0.2g})".format(*popt))
 
     ###beautification
-    ax.legend(loc=0, title=r'$\sigma=3 \quad X \in R^{1000, 2}$', fontsize=12)
+    ax.legend(loc=0, title=r'$\sigma=5 \quad X \in R^{1000, 2}$', fontsize=12)
     ax.set_ylabel("MSE")
     ax.set_xlabel("N")
     ax.grid()
@@ -50,4 +50,4 @@ if __name__ == '__main__':
     ###putting the plot
     plt.show()
 
-    fig1.savefig(os.path.join('plots', f'sigma=3_trees_N=50_test_size=0.5_000samples.png'))
+    fig1.savefig(os.path.join('plots', f'sigma=5_trees_N=100_test_size=095_1000samples.png'))
