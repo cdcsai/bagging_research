10c8aff214d2e60082e8ed4096019f0ecab6aeeb
charles <charles.dognin@verisk.com>
2019-05-22 17:37:19 -0400

    updating model av mlp

--- experiment_2.py
+++ experiment_2.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numba import njit
+from scipy.stats import semicircular
 
 
 @njit(fastmath=True, parallel=True)
@@ -27,9 +28,9 @@ def experiment_2(n_trials, N, n_samples_range):
                 print('Count trial #: ', i)
             # x = np.random.normal(0, 1, size=n)
             # x = np.random.uniform(-1, 1, size=n)
-            bernouilli = np.random.binomial(1, 0.5, size=n)
+            x = semicircular.rvs(size=n)
             # rademacher
-            x = 2 * bernouilli - 1
+            # x = 2 * bernouilli - 1
 
             # # Estimator MSE var
             var_x = (len(x) / (len(x) - 1)) * np.var(x)
@@ -61,6 +62,53 @@ def experiment_2(n_trials, N, n_samples_range):
         distance_array[n] = DIST
     return distance_array
 
+# @njit(fastmath=True, parallel=True)
+def experiment_2_biased(n_trials, N, n_samples_range):
+    distance_array = np.empty((n_samples_range + 1,))
+    TRUE_VAR = 1
+
+    for n in range(2, n_samples_range + 1):
+        print('Count epoch #: ', n)
+        total_var, total_var_bag = np.empty(n_trials, ), np.empty(n_trials, )
+        for i in range(n_trials):
+            # if i % 1000 == 0:
+            # print('Count trial #: ', i)
+            # x = np.random.normal(0, 1, size=n)
+            # x = np.random.uniform(-1, 1, size=n)
+            x = semicircular.rvs(size=n)
+            # rademacher
+            # x = 2 * bernouilli - 1
+
+            # # Estimator MSE var
+            var_x = np.var(x)
+            total_var[i] = var_x
+
+            # Estimator MSE var avec bagging
+            mean_bagg = np.empty(N, )
+            for j in range(N):
+                bag = bagging_np(x)
+                var_x_bag = np.var(bag)
+                mean_bagg[j] = var_x_bag
+            var_bag = np.mean(mean_bagg)
+            total_var_bag[i] = var_bag
+
+        EXP_var = np.mean(total_var)
+        EXP_var_bag = np.mean(total_var_bag)
+
+        BIAS_sq_var = (EXP_var - TRUE_VAR) ** 2
+        BIAS_sq_var_bag = (EXP_var_bag - TRUE_VAR) ** 2
+
+        VAR_VAR_bag = np.var(total_var_bag)
+        VAR_VAR = np.var(total_var)
+
+        MSE_VAR = VAR_VAR + BIAS_sq_var
+        MSE_VAR_BAG = VAR_VAR_bag + BIAS_sq_var_bag
+
+        DIST = (MSE_VAR - MSE_VAR_BAG)
+
+        distance_array[n] = DIST
+    return distance_array
+
 
 if __name__ == '__main__':
     import argparse
@@ -70,11 +118,16 @@ if __name__ == '__main__':
     parser.add_argument('--ds', type=str, default='def', help="Dataset")
     parser.add_argument('--n_feats', type=int, default=5, help="num features")
     parser.add_argument('--n_average', type=int, default=50, help="Number or average")
-    parser.add_argument('--n_samples', type=int, default=100, help="NUmber of samples")
+    parser.add_argument('--n_samples', type=int, default=100, help="Number of samples")
     parser.add_argument('--n_train_iter', type=int, default=10, help="Number of train/test loop")
     parser.add_argument('--N', type=int, default=200, help="Number of train/test loop")
 
-    distance_arr = experiment_2(n_trials=50000, N=50, n_samples_range=50)
+    # distance_arr = experiment_2(n_trials=50000, N=50, n_samples_range=50)
+    # with open('distance_array.txt', 'w') as f:
+    #     for el in distance_arr[2:]:
+    #         f.write(str(el) + '\n')
+
+    distance_arr_semi_circ = experiment_2_biased(n_trials=1000, N=50, n_samples_range=50)
     with open('distance_array.txt', 'w') as f:
-        for el in distance_arr[2:]:
+        for el in distance_arr_semi_circ[2:]:
             f.write(str(el) + '\n')
--- plot_experiment_2.py
+++ plot_experiment_2.py
@@ -1,21 +1,22 @@
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     import numpy as np
-    import pandas as pd
 
-    data = pd.read_csv('/Users/charlesdognin/Desktop/bagging_research_/old_res/rademacher_dist.csv')
-    x, y = data['x'][7:], data['y'][7:]
-    #
+    data = open('distance_array.txt').readlines()
+    # x, y = data['x'][7:], data['y'][7:]
+
     # x1 = np.arange(2, 51)
-    # x2= np.arange(1, 51)
-    # y1 = [float(el.strip('\n')) for el in data]
+
+    y = [float(el.strip('\n')) for el in data]
+    x = np.arange(1, len(y) + 1)
+
     ###preparing the figure
     fig1 = plt.figure(1)
     ax=fig1.add_subplot(1, 1, 1)
 
     ###the three sets of data to plot
     ax.plot(x, y, linestyle='', marker='o', color='r', label=r'$MSE(\hat{\sigma},n)-MSE(\sigma,n)$')
-    ax.plot(x, list(map(lambda x: (1 / x**2), x)), color='b', label=r'$\frac{-2\mu_4+3V^2}{n^2}$')
+    ax.plot(x, list(map(lambda x: (-(1 / 16) / x**2), x)), color='b', label=r'$\frac{-2\mu_4+3V^2}{n^2}$')
 
     ###beautification
     ax.legend(loc=0, fontsize=12)
