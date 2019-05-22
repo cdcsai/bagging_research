274f1aa39c7285ba69e618ff69ed69563a2b6083
charlesdognin <charles.dognin@polytechnique.edu>
2019-05-22 17:38:31 -0400

    Merge branch 'master' of https://github.com/cdcsai/bagging_research
    # Conflicts:
    #	plot_experiment_3.py

--- plot_experiment_3.py
+++ plot_experiment_3.py
@@ -40,4 +40,4 @@ def plot_kurt(path, save_path='plots'):
 
 
 if __name__ == '__main__':
-    plot_kurt('res_special_rad__a=0.1_trials=10000_N=20_n=10.txt', save_path='plots')
+    plot_kurt('res_special_rad__a=0.1_trials=100000_N=20_n=10.txt', save_path='plots')
